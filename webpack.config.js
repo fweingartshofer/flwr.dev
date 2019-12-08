6eceaa83128210d142bd3fea8dabacb16c925a93
florian.weingartshofer <florian.weingartshofer@gmail.com>
2019-12-08 14:37:30 +0100

    Updated site

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,44 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 
 module.exports = {
-    plugins: [
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // all options are optional
-            filename: '[name].css',
-            chunkFilename: '[id].css',
-            ignoreOrder: false, // Enable to remove warnings about conflicting order
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.less$/,
-                use: [
-                    {
-                      loader: MiniCssExtractPlugin.loader,
-                      options: {
-                        publicPath: './',
-                        hmr: true,
-                        sourceMap: true,
-                        minimize: true
-                      }
-                    },
-                    {
-                        loader: 'css-loader', // translates CSS into CommonJS
-                        options: {
-                            sourceMap: true,
-                        }
-                    },
-                    {
-                        loader: 'less-loader', // compiles Less to CSS
-                        options: {
-                            sourceMap: true,
-
-                        }
-                    },
-                ],
-            },
-        ],
-    },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+      ignoreOrder: false // Enable to remove warnings about conflicting order
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "./",
+              hmr: true,
+              sourceMap: true,
+              minimize: true
+            }
+          },
+          {
+            loader: "css-loader", // translates CSS into CommonJS
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: "less-loader", // compiles Less to CSS
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  }
 };
